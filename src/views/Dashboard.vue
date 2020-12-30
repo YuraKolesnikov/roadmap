be153a7e19c35932341ca3a017b5ac5851136a1f
YuraKolesnikov <cezar278@inbox.lv>
2020-12-30 15:30:04 +0700

    Added computed booleans for code readability and reusability

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,9 @@
   <div class="about">
     Dashboard
     <p>{{userRole}}</p>
+    <p>Is admin? {{isAdmin}}</p>
+    <p>Is mentor? {{isMentor}}</p>
+    <p>Is user? {{isUser}}</p>
   </div>
 </template>
 
@@ -13,6 +16,15 @@ export default {
     ...mapState('auth', ['userStatus']),
     userRole() {
       return UserModel.ID_TO_DATA[this.userStatus]
+    },
+    isAdmin() {
+      return this.userStatus === UserModel.ADMIN.id
+    },
+    isMentor() {
+      return this.userStatus === UserModel.MENTOR.id
+    },
+    isUser() {
+      return this.userStatus === UserModel.USER.id
     }
   }
 }
