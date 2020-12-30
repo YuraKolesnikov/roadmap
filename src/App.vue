4dd528fdbe133c44f2b0203326856d236085ee89
YuraKolesnikov <cezar278@inbox.lv>
2020-12-30 18:09:58 +0700

    Added styling

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/dashboard">Dashboard</router-link> |
-      <router-link to="/sign-in">Sign in</router-link>
+    <div class="navbar-fixed">
+      <nav style="padding: 0 10px">
+        <div class="nav-wrapper">
+          <router-link class="brand-logo" to="/">Roadmap App</router-link>
+          <ul class="right hide-on-med-and-down">
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/dashboard">Dashboard</router-link></li>
+            <li><router-link to="/sign-in">Sign in</router-link></li>
+          </ul>
+        </div>
+      </nav>
     </div>
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapState } from 'vuex'
+
+export default {
+
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,42 +1,53 @@
 <template>
   <div class="about">
-    Dashboard
     <!--<p>{{userRole}}</p>
     <p>Is admin? {{isAdmin}}</p>
     <p>Is mentor? {{isMentor}}</p>
     <p>Is user? {{isUser}}</p>-->
     <p>User: {{ fullName }}</p>
     <div class="plans">
-      <div class="plan" v-for="plan in plans" :key="`plan_${plan.id}`">
-        <h4 class="plan__title">{{ plan.title }}</h4>
-        <ul class="steps">
-          <li class="steps__item" v-for="(step, i) in plan.steps" :key="`plan_${plan.id}_step_${step.id}`">
-            <h5>Step {{ ++i }}: {{ step.title }} ({{ StatusModel.ID_TO_DATA[step.status].title }})</h5>
-            <p v-if="step.startDate && step.endDate">Start date: {{ step.startDate }}. End date: {{ step.endDate }}</p>
-            <ul class="skills">
-              <li v-for="(skill, i) in step.skills" :key="`plan_${plan.id}_step_${step.id}_skill_${i}`">
-                <template v-if="skill.href">
-                  <a target="_blank" rel="noreferrer" :href="skill.href">{{ skill.title }}</a>
-                </template>
-                <template v-else>{{ skill.title }}</template>
-              </li>
-            </ul>
-            <button @click="startStep(plan.id, step.id)" v-if="step.status === StatusModel.NEW.id">Start</button>
-            <button @click="testStep(plan.id, step.id)" v-if="step.status === StatusModel.IN_PROGRESS.id">Test</button>
-          </li>
-        </ul>
+      <div class="plan card" v-for="plan in plans" :key="`plan_${plan.id}`">
+        <div class="card-content">
+          <h4 class="card-title">{{ plan.title }}</h4>
+          <ul>
+            <li class="card" v-for="(step, i) in plan.steps" :key="`plan_${plan.id}_step_${step.id}`">
+              <div class="card-content">
+                <h5
+                  style="cursor: pointer"
+                  class="card-title"
+                  @click.self="toggleCollapse(step.id)">
+                  Step {{ ++i }}: {{ step.title }} <span :class="getBadgeClass(step.status)" class="badge">{{ StatusModel.ID_TO_DATA[step.status].title }}</span>
+                </h5>
+                <p v-if="step.startDate && step.endDate">Start date: {{ step.startDate }}. End date: {{ step.endDate }}</p>
+                <ul class="collection" v-if="openTab === step.id">
+                  <li class="collection-item" v-for="(skill, i) in step.skills" :key="`plan_${plan.id}_step_${step.id}_skill_${i}`">
+                    <template v-if="skill.href">
+                      <a target="_blank" rel="noreferrer" :href="skill.href">{{ skill.title }}</a>
+                    </template>
+                    <template v-else>{{ skill.title }}</template>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-action" v-if="openTab === step.id">
+                <button class="btn" @click="startStep(plan.id, step.id)" v-if="step.status === StatusModel.NEW.id">Start</button>
+                <button class="btn" @click="testStep(plan.id, step.id)" v-if="step.status === StatusModel.IN_PROGRESS.id">Test</button>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import UserModel from '@/models/UserModel';
+import UserModel from 'Models/UserModel';
 import { mapState, mapMutations } from 'vuex'
-import StatusModel from '@/models/StatusModel';
+import StatusModel from 'Models/StatusModel';
 export default {
   data() {
     return {
+      openTab: null,
       UserModel,
       StatusModel
     }
@@ -50,6 +61,22 @@ export default {
     },
     testStep(planId, stepId) {
       this.SET_STEP_STATUS({ planId, stepId, newStatus: StatusModel.TESTING.id })
+    },
+    toggleCollapse(id) {
+      this.openTab = id
+    },
+    getBadgeClass(status) {
+      if (status === StatusModel.NEW.id) {
+        return 'purple darken-1 white-text'
+      } else if (status === StatusModel.IN_PROGRESS.id) {
+        return 'yellow darken-1 white-text'
+      } else if (status === StatusModel.TESTING.id) {
+        return 'cyan darken-2 white-text'
+      } else if (status === StatusModel.PASSED.id) {
+        return 'green darken-2 white-text'
+      } else {
+        return 'red white-text'
+      }
     }
   },
   computed: {
